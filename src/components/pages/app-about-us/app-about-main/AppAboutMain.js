eafe1549218eaf2658300adfe2faa0e85ce08886
iforcie <ilyacool3@gmail.com>
2022-08-12 10:41:06 +0300

    deleting footer

--- src/components/pages/app-about-us/app-about-main/AppAboutMain.js
+++ src/components/pages/app-about-us/app-about-main/AppAboutMain.js
@@ -3,17 +3,15 @@ import {Component} from 'react';
 import AppAboutCoffee from '../app-about-coffee/AppAboutCoffee';
 import AppGetMoreAbout from '../app-get-more-about/AppGetMoreAbout';
 import AppOurBest from '../app-our-best/AppOurBest';
-import AppFooter from '../../../app-footer/AppFooter';
 
 class AppAboutMain extends Component {
 	render() {
 		return (
-			<div>
+			<>
 				<AppGetMoreAbout />
 				<AppAboutCoffee />
 				<AppOurBest />
-				<AppFooter />
-			</div>
+			</>
 		)
 	}
 }
