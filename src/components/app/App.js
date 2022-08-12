198d7d80013dd52c119c07f38e7f6967d5cff808
iforcie <ilyacool3@gmail.com>
2022-08-12 10:41:19 +0300

    adding footer

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,7 @@
 import { Component } from 'react';
 
-// import Header from '../app-header/AppHeader';
 import AppAboutMain from '../pages/app-about-us/app-about-main/AppAboutMain';
-// import AppAbout from '../pages/app-about-us/app-about-coffee/AppAbout';
-// import AppOurBest from '../pages/app-about-us/app-our-best/AppOurBest';
-// import AppGetMoreAbout from '../pages/app-about-us/app-get-more-about/AppGetMoreAbout';
-// import AppFooter from '../app-footer/AppFooter';
+import AppFooter from '../app-footer/AppFooter';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -14,6 +10,7 @@ class App extends Component {
     return (
       <div className="app">
         <AppAboutMain />
+        <AppFooter />
       </div>
     );
   }
